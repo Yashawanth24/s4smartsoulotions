66a7d0a4d22206c810638bb318520523dabfbbc0
Yashawanth <yashawanthbg@gmail.com>
2024-09-12 11:24:00 +0530

    mediaquires

--- src/HeroSection.js
+++ src/HeroSection.js
@@ -5,20 +5,34 @@ const HeroSection = () => {
     <div className="container-fluid text-white p-5" id="body" style={{ backgroundColor: 'black', height: '100vh' }}>
       <div className="row mt-5">
         <div className="col-md-1"></div>
-        <div className="col-md-5 position-relative d-flex justify-content-start align-items-start rounded-lg">
-          <img
-            src="image/hero1.jpg"
-            alt="Illustration"
-            className="img-fluid position-absolute moving-image hide-on-mobile"
-            style={{ maxWidth: '60%', right: '80%', bottom: '-4%', transform: 'rotate(-15deg)', borderRadius: '20px' }}
-          />
-          <img
-            src="image/hero2.jpg"
-            alt="Sticker"
-            className="img-fluid position-absolute moving-image hide-on-mobile"
-            style={{ maxWidth: '45%', right: '28%', bottom: '-54%', height:"300px", transform: 'rotate(18deg)', borderRadius: '20px' }}
-          />
-        </div>
+        <div className="col-md-5 position-relative d-flex justify-content-start align-items-start rounded-lg image-container">
+  <img
+    src="image/hero1.jpg"
+    alt="Illustration"
+    className="img-fluid position-absolute moving-image hide-on-mobile"
+    style={{
+      maxWidth: '60%',
+      right: '80%',
+      bottom: '-4%',
+      transform: 'rotate(-15deg)',
+      borderRadius: '20px'
+    }}
+  />
+  <img
+    src="image/hero2.jpg"
+    alt="Sticker"
+    className="img-fluid position-absolute moving-image hide-on-mobile sticker"
+    style={{
+      maxWidth: '45%',
+      right: '28%',
+      bottom: '-54%',
+      height: '300px',
+      transform: 'rotate(18deg)',
+      borderRadius: '20px'
+    }}
+  />
+</div>
+
 
         <div className="col-md-6 d-flex flex-column justify-content-center align-items-start text-start custom-text-section">
           <h1 className="main-heading" style={{ fontSize: '3.6rem', fontWeight: 'bold', lineHeight: '1.2', marginBottom: '1rem', textAlign: 'left' }}>
