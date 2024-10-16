f5f676254f89a6f5fb3debe6e072044ff56977b2
Yashawanth <yashawanthbg@gmail.com>
2024-10-16 10:27:15 +0530

    cssupdated

--- src/IndivdualServices.js
+++ src/IndivdualServices.js
@@ -10,6 +10,11 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/660c0f4206348c58b1365a69_social-media-advertising-services-features-p-500.webp",
     h3: "Connect with the right audiences through social ads",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/660c0f4206348c58b1365a69_social-media-advertising-services-features.webp",
+    accordionContent: [
+      { title: 'Get the best results for your business', content: 'Optimize your budget with cross-media optimization. Our XMO technology lets you set one budget for your business goals, adjusting spend between channels based on real-time performance.' },
+      { title: 'Have everything you need in one place', content: 'See performance and results from your social ad campaigns across multiple platforms in one unified marketing dashboard.' },
+      { title: 'Know your campaigns are in good hands', content: 'You deserve a partner that’s committed to delivering excellence. Our experts can help you run great ads across Facebook, Instagram, Snapchat, and more.' },
+    ]
   },
   displayads: {
     title: "Display Ads",
@@ -17,6 +22,12 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628b168ef8096e539023b2e_display-advertising-services-performance-dashboard-p-800.webp",
     h3: "Maximize your ROI with display ads",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/66289cc0cedfaf4f4a0f4b29_display-advertising-services-features-2.webp",
+    accordionContent: [
+      { title: 'Get your message delivered', content: 'Your display ads will run across the web where people are spending time online, helping you reach hundreds of millions of unique users each month.' },
+      { title: 'Gain exposure for your business', content: 'Reach more audiences faster. Our networks give your business access to 1 out of 2 adults in the US.' },
+      { title: 'See how your ads are performing', content: 'Measure and track your display campaign performance and leads with our marketing dashboard.' },
+      { title: 'See how your ads are performing', content: 'Measure and track your display campaign performance and leads with our marketing dashboard.' },
+    ]
   },
   searchads: {
     title: "Search Ads",
@@ -24,6 +35,12 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628ec625cf5484477abd8ef_ppc-management-services-dashboard-p-500.webp",
     h3: "Get more leads for your business",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628ec25085dc1231bf1fa32_ppc-management-services-features.webp",
+    accordionContent: [
+      { title: 'Experience the power of XMO', content: 'Optimize your search ad campaigns with help from our cross-media optimization technology. XMO enables goal-based advertising using insights, optimizations, and actions to deliver results.' },
+      { title: 'Maximize your budget', content: 'Get real leads, not just clicks. Our platform uses historical data to continuously optimize your Google ads and other PPC campaigns over time, finding the right keywords, bids, and placements for your business.' },
+      { title: 'Know exactly how your campaigns are performing', content: 'View real-time data like site visits, calls, emails, and forms submitted with online and mobile reports through our lead dashboard.' },
+      { title: 'Put your trust in us', content: 'Get the latest and greatest insights for your business. We have leading partnerships with Google, Yahoo, and Microsoft that will help you grow.' },
+    ]
   },
   listings: {
     title: "Listings",
@@ -31,6 +48,12 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628f798dfe0688fee2bd632_local-business-listing-management-tool-features-p-500.webp",
     h3: "Managing your business listings is easier than ever",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628f798dfe0688fee2bd632_local-business-listing-management-tool-features.webp",
+    accordionContent: [
+      { title: 'Update all your listings with one click', content: 'Your business listings can be easily managed across up to 50+ online directories, including Google Business Profile, Apple Maps, Bing, Yelp, and more—all from one master listing in a single dashboard.' },
+      { title: 'Get easy-to-read dashboard reports', content: 'See how your local business listings are performing, make updates and review your online info, and view reports on how people are getting to your listings.' },
+      { title: 'Increase your business’s visibility', content: 'Be seen by the right audiences. Your directory listings show up in search results when it counts, so you can attract new customers.' },
+      { title: 'Stay on top of your reviews', content: 'See your average star rating and most recent reviews, then easily respond to them with our all-in-one platform.' },
+    ]
   },
   websites: {
     title: "Website and Landing Pages",
@@ -38,6 +61,12 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628f7ba2038a5f4fad63a46_small-business-website-builder-site-security-p-500.webp",
     h3: "Set your website up for success",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628f7bb1312e259fc83eda0_small-business-website-builder-site-design-desktop-mobile.webp",
+    accordionContent: [
+      { title: 'Increase your conversions', content: 'Convert your website visitors into leads through click to call, web forms, call tracking, and more.' },
+      { title: 'Understand what’s working', content: 'See what leads people to your site and what drives them to contact you with our tracking and capture technology.' },
+      { title: 'Get everything you need in one place', content: 'Track your leads, listen to recorded phone calls, and follow up directly from our marketing dashboard.' },
+      { title: 'Ensure your website is secure and user-friendly', content: 'Get a mobile-optimized and SEO-friendly site along with an SSL certificate to show search engines it’s safe.' },
+    ]
   },
   emailmarketing: {
     title: "Targeted Email Marketing",
@@ -45,6 +74,11 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/660c0f4206348c58b1365a69_social-media-advertising-services-features-p-500.webp",
     h3: "Connecting you to the right people",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6629eb3ab22f996542016c85_email-marketing-services-business-features.webp",
+    accordionContent: [
+      { title: 'Reach your intended audience', content: 'We follow proven best practices and target prospects by location, life event, and demographics to make sure your emails hit the mark.' },
+      { title: 'Improve your campaign results', content: 'Send your message to people who actually want to hear from you. Our list-verification practices ensure emails are being delivered to your most likely customers.' },
+      { title: 'Get tailored business solutions', content: 'Track your leads, listen to recorded phone calls, and follow up directly from our marketing dashboard.' },
+    ]
   },
   seo: {
     title: "SEO",
@@ -52,6 +86,11 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628b168ef8096e539023b2e_display-advertising-services-performance-dashboard-p-800.webp",
     h3: "Increase your business success with local SEO",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6629e89b296855116b41165f_small-business-seo-services-features.webp",
+    accordionContent: [
+      { title: 'Optimize your site performance', content: 'We use strategic SEO keywords, topics, and tactics to help your business perform better in search results.' },
+      { title: 'Improve visibility', content: 'See what leads people to your site and what drives them to contact you with our tracking and capture technology.' },
+      { title: 'See real results', content: 'Get a view of new leads and traffic sources, discover which pages and search queries drive the most clicks, and more.' },
+    ]
   },
   socialmedia: {
     title: "Social Media Marketing",
@@ -59,6 +98,11 @@ const servicesData = {
     imageUrl: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/6628ec625cf5484477abd8ef_ppc-management-services-dashboard-p-500.webp",
     h3: "Build a better social media presence for your small business",
     keyfeaturesimage: "https://cdn.prod.website-files.com/6604adbd42b73cba937072b0/66289cc0cedfaf4f4a0f4b29_display-advertising-services-features-2.webp",
+    accordionContent: [
+      { title: 'Increase social engagement', content: 'Grow your reach with consistent, relevant posts that drive interactions with followers.' },
+      { title: 'Manage your reviews', content: 'Maintain a strong online reputation. Our platform uses brand listening to find active conversations related to your business and then helps you respond.' },
+      { title: 'Do it all in less time', content: 'There are never enough hours in your day. We’ll help manage your primary social media channels and review feedback, so you can focus on your customers.' },
+    ]
   },
   drupaldevelopment: {
     title: "Drupal Development Services",
@@ -162,30 +206,32 @@ const IndivdualServices = () => {
       <section className="dashboard-content container">
         <div className="row">
           <div className="text-content1 col-md-12">
-  
-            {paragraphs.map((para, index) => (
-              <div key={index}>
-                <p
-                  className="subheading1 lead"
-                  onClick={() => toggleAccordion(index)}
-                  style={{
-                    cursor: "pointer",
-                    color: openIndex === index ? "#befb7c" : "white",
-                    fontWeight: "bold"
-                  }}
-                >
-                  {para.title}
-                </p>
-                {openIndex === index && (
-                  <div className="accordion-content">
-                    <p className="info-text">{para.content}</p>
-                  </div>
-                )}
-              </div>
-            ))}
+            {service.accordionContent &&
+              service.accordionContent.map((item, index) => (
+                <div key={index}>
+                  <p
+                    className="subheading1 lead"
+                    onClick={() => toggleAccordion(index)}
+                    style={{
+                      cursor: "pointer",
+                      color: openIndex === index ? "#befb7c" : "white",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {item.title}
+                  </p>
+                  {openIndex === index && (
+                    <div className="accordion-content">
+                      <p className="info-text">{item.content}</p>
+                    </div>
+                  )}
+                </div>
+              ))}
           </div>
         </div>
       </section>
+
+
     </div>
 
 
